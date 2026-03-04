e31bb5a33a550b0c45296d23e8bd3a5a301702a1
vvcherkasov <cherkasov612@yandex.ru>
2026-03-04 23:29:46 +0300

    Практикуемся в работе с HTTPX

---
+++ httpx_delete_user.py
@@ -0,0 +1,32 @@
+import httpx
+from tools.fakers import get_random_email
+
+create_user_payload = {
+  "email": get_random_email(),
+  "password": "string",
+  "lastName": "string",
+  "firstName": "string",
+  "middleName": "string"
+}
+
+create_user_response = httpx.post("http://127.0.0.1:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+print("Create user data:", create_user_response_data)
+
+login_payload = {
+  "email": create_user_payload["email"],
+  "password": create_user_payload["password"]
+}
+
+login_response = httpx.post("http://127.0.0.1:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print("Login data:", login_response_data)
+
+delete_user_headers = {
+  "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
+}
+delete_user_response = httpx.delete(
+  f"http://127.0.0.1:8000/api/v1/users/{create_user_response_data['user']['id']}",
+  headers=delete_user_headers
+)
+print("Delete user status code:", delete_user_response.status_code)
---
+++ httpx_get_user.py
@@ -0,0 +1,34 @@
+
+import httpx
+from tools.fakers import get_random_email
+
+create_user_payload = {
+  "email": get_random_email(),
+  "password": "string",
+  "lastName": "string",
+  "firstName": "string",
+  "middleName": "string"
+}
+
+create_user_response = httpx.post("http://127.0.0.1:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+print("Create user data:", create_user_response_data)
+
+login_payload = {
+  "email": create_user_payload["email"],
+  "password": create_user_payload["password"]
+}
+
+login_response = httpx.post("http://127.0.0.1:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print("Login data:", login_response_data)
+
+get_user_headers = {
+  "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
+}
+get_user_response = httpx.get(
+  f"http://127.0.0.1:8000/api/v1/users/{create_user_response_data['user']['id']}",
+  headers=get_user_headers
+)
+get_user_response_data = get_user_response.json()
+print("Get user data:", get_user_response_data)
