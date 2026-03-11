7054edee03dc03a4309da48a17857c801414c2a1
vvcherkasov <cherkasov612@yandex.ru>
2026-03-11 23:08:45 +0300

    Практикуемся в реализации API клиентов

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,27 @@
+from clients.api_client import APIClient
+from httpx import Response
+from typing import TypedDict
+
+
+class CreateUserRequestDict(TypedDict):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с /api/v1/users
+    """
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
+        """
+        Метод выполняет создание пользователя.
+
+        :param request: Словарь с данными пользователя.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post("/api/v1/users", json=request)
