55dd5e2815cb2aafb51cb30760ad093fdb5b5906
vvcherkasov <cherkasov612@yandex.ru>
2026-03-23 16:49:48 +0300

    Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field, EmailStr
+
+class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserSchema
