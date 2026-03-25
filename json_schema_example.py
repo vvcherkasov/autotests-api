88b87a3cd4ad6ee030f51565394d8fd102307c58
vvcherkasov <cherkasov612@yandex.ru>
2026-03-25 22:49:53 +0300

    Валидация JSON schema

---
+++ json_schema_example.py
@@ -0,0 +1,17 @@
+from jsonschema import validate
+
+schema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "age": {"type": "number"}
+    },
+    "required": ["name"]
+}
+
+data = {
+    "name": "Alice",
+    "age": 30
+}
+
+validate(instance=data, schema=schema)
---
+++ pydantic_json_schema_create_user.py
@@ -0,0 +1,24 @@
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from tools.assertions.schema import validate_json_schema
+from tools.fakers import get_random_email
+import jsonschema
+
+# Инициализируем клиент PublicUsersClient
+public_users_client = get_public_users_client()
+
+# Инициализируем запрос на создание пользователя
+create_user_request = CreateUserRequestSchema(
+    email=get_random_email(),
+    password="string",
+    last_name="string",
+    first_name="string",
+    middle_name="string"
+)
+
+# Отправляем POST запрос на создание пользователя
+create_user_response = public_users_client.create_user_api(create_user_request)
+create_user_response_json = create_user_response.json()
+create_user_response_schema = CreateUserResponseSchema.model_json_schema()
+
+validate_json_schema(instance=create_user_response_json, schema=create_user_response_schema)
