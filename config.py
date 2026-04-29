2dbbad517d9149f7b6a1ed4772e1d1608879a4ff
vvcherkasov <cherkasov612@yandex.ru>
2026-04-29 10:55:41 +0300

    Добавляем информацию о переменных окружения в Allure отчет

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, HttpUrl, FilePath
+from pydantic import BaseModel, HttpUrl, FilePath, DirectoryPath
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Self
 
 class HTTPClientConfig(BaseModel):
     url: HttpUrl
@@ -22,7 +23,14 @@ class Settings(BaseSettings):
 
     test_data: TestDataConfig
     http_client: HTTPClientConfig
+    allure_results_dir: DirectoryPath
 
+    @classmethod
+    def initialize(cls) -> Self:
+        allure_results_dir = DirectoryPath("./allure-results")
+        allure_results_dir.mkdir(exist_ok=True)
 
-settings = Settings()
+        return Settings(allure_results_dir=allure_results_dir)
+
+settings = Settings.initialize()
 
