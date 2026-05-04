4c4b19328f931ae6d2df44c5c71182a7255164ea
vvcherkasov <vvcherkasov@users.noreply.github.com>
2026-05-04 12:14:04 +0000

    deploy: 0bd3e171e840b7cdbfbc51a5dc17ed12f0b61e91

--- fixtures/authentication.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from clients.authentication.authentication_client import get_authentication_client, AuthClient
-
-@pytest.fixture
-def authentication_client() -> AuthClient:
-    return get_authentication_client()
